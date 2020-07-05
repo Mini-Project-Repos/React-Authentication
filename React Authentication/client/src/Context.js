ee57d98bbfe0eea4e3c6567f82f5732e31d833b8
Jamie Gobeille <djent34@gmail.com>
2020-07-05 12:12:59 +0900

    updated

--- React Authentication/client/src/Context.js
+++ React Authentication/client/src/Context.js
@@ -1,35 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Data from "./Data";
 
-const Context = React.createContext(); 
+const Context = React.createContext();
+
+/**
+ * The Provider class in the file Context.js is a "higher-order component"
+ *
+ * @returns a Provider component which provides the application state and any actions or event handlers
+ * that need to be shared between components, via a required value prop.
+ */
 
 export class Provider extends Component {
-
   constructor() {
     super();
+    this.data = new Data();
   }
 
   render() {
+    const value = {
+      data: this.data,
+    };
+
+    console.log(value);
+
     return (
-      <Context.Provider>
-        {this.props.children}
-      </Context.Provider>  
+      //value represents an object containing the context to be shared throughout the component tree.
+      <Context.Provider value={value}>{this.props.children}</Context.Provider>
     );
   }
 
-  
-  signIn = async () => {
+  signIn = async () => {};
 
-  }
-
-  signOut = () => {
-
-  }
+  signOut = () => {};
 }
 
 export const Consumer = Context.Consumer;
 
 /**
  * A higher-order component that wraps the provided component in a Context Consumer component.
+ *
+ * withContext automatically subscribes (or connects) the component passed to it to all actions and context changes
+ *
  * @param {class} Component - A React component.
  * @returns {function} A higher-order component.
  */
@@ -38,9 +49,8 @@ export default function withContext(Component) {
   return function ContextComponent(props) {
     return (
       <Context.Consumer>
-        {context => <Component {...props} context={context} />}
+        {(context) => <Component {...props} context={context} />}
       </Context.Consumer>
     );
-  }
+  };
 }
-
